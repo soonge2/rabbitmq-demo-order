de7251662a0482b4ea588fd31a048a2522978030
unknown <jiwonsoong13@gmail.com>
2025-10-21 18:37:29 +0900

    first commit

---
+++ src/main/java/com/example/rabbitmqdemoorder/RabbitmqDemoOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqdemoorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqDemoOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqDemoOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rabbitmqdemoorder/RabbitmqDemoOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqdemoorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqDemoOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
